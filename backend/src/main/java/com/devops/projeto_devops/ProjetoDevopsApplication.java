bffc8bf6adac88c856ba7aa4622620dd041a71f2
André Figueiredo <andre.figueiredo@jointecnologia.com.br>
2025-09-20 12:41:26 -0300

    adicionando pasta de backend

---
+++ backend/src/main/java/com/devops/projeto_devops/ProjetoDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.devops.projeto_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevopsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devops/projeto_devops/ProjetoDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.projeto_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
