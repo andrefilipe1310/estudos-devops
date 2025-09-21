3c1fb107dd28faf4b5d899f2383d165f529f8c13
André Figueiredo <andre.figueiredo@jointecnologia.com.br>
2025-09-20 21:57:54 -0300

    adicionando rota get

--- backend/src/main/java/com/devops/projeto_devops/controller/UserController.java
+++ backend/src/main/java/com/devops/projeto_devops/controller/UserController.java
@@ -3,6 +3,7 @@ package com.devops.projeto_devops.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-
+    @GetMapping
     public ResponseEntity<String> getUsers() {
         return ResponseEntity.status(HttpStatus.OK).body("List of users alterada");
     }
